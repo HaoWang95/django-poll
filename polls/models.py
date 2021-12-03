5529d03d877cd20595f252deb20fc90395cff070
HaoWang95 <alanwang0028@gmail.com>
2021-12-04 01:08:19 +1100

    init

---
+++ polls/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date created')
+    up_date = models.DateTimeField('date updated', default=timezone.now)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    is_correct_choice = models.BooleanField(default=False)
+    votes = models.IntegerField(default=0)
