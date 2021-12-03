9528fdd75e8d68fffece3fdeaf7375ee39e36f98
HaoWang95 <alanwang0028@gmail.com>
2021-12-04 02:05:45 +1100

    model update

--- polls/models.py
+++ polls/models.py
@@ -7,9 +7,15 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date created')
     up_date = models.DateTimeField('date updated', default=timezone.now)
 
+    def __str__(self) -> str:
+        return self.question_text
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     is_correct_choice = models.BooleanField(default=False)
     votes = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.choice_text
