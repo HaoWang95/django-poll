c19971caac9a82afdeeb1076270fbaa1627fc5a0
HaoWang95 <alanwang0028@gmail.com>
2021-12-05 12:02:40 +1100

    last modified update

--- polls/models.py
+++ polls/models.py
@@ -1,11 +1,12 @@
+from typing import Tuple
 from django.db import models
 from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date created')
-    up_date = models.DateTimeField('date updated', default=timezone.now)
+    pub_date = models.DateTimeField(auto_now=True)
+    last_modified = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
         return self.question_text
@@ -16,6 +17,8 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     is_correct_choice = models.BooleanField(default=False)
     votes = models.IntegerField(default=0)
+    pub_date = models.DateTimeField(auto_now=True)
+    last_modified = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
         return self.choice_text
